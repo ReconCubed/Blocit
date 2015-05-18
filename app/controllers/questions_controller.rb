47371bc666718b0e4f984fa71e59cd6687264b12
Sam Stevens <recongamingsam@gmail.com>
2015-05-18 21:32:34 +0000

    Questions COmpleted

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,4 @@
-class QuestionController < ApplicationController
+class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
@@ -20,7 +20,7 @@ class QuestionController < ApplicationController
   end
 
   def show
-    @questions = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def edit
