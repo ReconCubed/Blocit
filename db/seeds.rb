810881b13d2d3618e9fc78d73326dcda30ef19dc
Sam Stevens <recongamingsam@gmail.com>
2015-06-24 12:33:04 +0000

    Topics and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,7 @@ users = User.all
 50.times do
     Post.create!(
         user: users.sample,
+        topic: topics.sample,
         title: Faker::Hacker.abbreviation,
         body: Faker::Hacker.say_something_smart
         )
@@ -44,9 +45,39 @@ users = User.all
     
 
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'me@reconcubed.me',
-   password: 'helloworld'
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+admin.skip_confirmation!
+admin.save!
+ 
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save!
+ 
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
+)
+member.skip_confirmation!
+member.save!
+ 
+15.times do
+ Topic.create!(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
  )
+end
+topics = Topic.all
