00d7f1150c48ef018a0b5b9776032166b143ddd9
Sam Stevens <recongamingsam@gmail.com>
2015-06-24 10:30:21 +0000

    Auth

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    authorize @post
+    authorize @posts
   end
   def create
     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
@@ -26,10 +26,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    authorize @post
   end
   
   def update
     @post = Post.find(params[:id])
+    authorize @post
     if @post.update_attributes(params.require(:post).permit(:title, :body))
       flash[:notice] = "Post was updated."
       redirect_to @post
@@ -39,3 +41,4 @@ class PostsController < ApplicationController
     end
   end
 end
+
