2eb04d94c6ae95ce22649f8ca33b1fea7b739f62
Sam Stevens <recongamingsam@gmail.com>
2015-08-31 20:08:15 +0000

    Destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,20 @@ class PostsController < ApplicationController
     @comments = @post.comments 
     @comment = @post.comments.build
   end
-
+  
+   def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+ 
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+  end
   def new
     @topic = Topic.find(params[:topic_id])
     @post = Post.new
--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -5,4 +5,7 @@ class CommentPolicy < ApplicationPolicy
   def new?
       user.present?
   end
+  def destroy?
+    user.present? && (record.user == user || user.admin? || user.moderator?)
+  end
 end
